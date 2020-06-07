ed3a3fdc17a05d04715285732b5e3a5ad07e1431
doreamon95 <doreamon95@163.com>
2020-06-07 23:29:00 +0800

    更新

--- first.sh
+++ first.sh
@@ -3,7 +3,7 @@ git init
 git add .
 git commit -m "更新"
 git remote add origin https://github.com/doreamon95/myblog.git
-git pull origin master 
+git pull origin master -f
 git push origin master
 
 # 报错执行强制更新 push origin master -f
