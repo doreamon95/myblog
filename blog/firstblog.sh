62dcbfe2fd4954c69c8f240b75006fa561432fc7
doreamon95 <doreamon95@163.com>
2020-07-15 18:02:39 +0800

    backup

--- blog/firstblog.sh
+++ blog/firstblog.sh
@@ -5,5 +5,5 @@ git init
 git add .
 git commit -m "pages "
 git remote add origin https://github.com/doreamon95/doreamon95.github.io.git
-git pull origin master 
-git push origin master
+# git pull origin master 
+git push origin master -f
